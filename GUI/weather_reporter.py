a0955b9f5598acc4d4284271c07d995aa844c44a
Santi24Yt <santi24yt@gmail.com>
2023-10-08 19:17:52 -0600

    snack processing

--- GUI/weather_reporter.py
+++ GUI/weather_reporter.py
@@ -244,6 +244,12 @@ class WeatherSearcher(UserControl):
 
             # Se limpian las entradas previas con cada búsqueda.
             search_query.disabled = True
+            if cache.act_runs.is_set():
+                self.page.show_snack_bar(SnackBar(content=
+                Text("Procesando...",
+                color= CLOUD_WHITE),
+                    open=True, bgcolor= NAVY_BLUE, duration= 8000,
+                    show_close_icon=True, close_icon_color= True,))
             self.update()
             search_results_grid.clean()
 
